8a55c44084de23f78377e0283976acfed4e63963
Fuzhong Zhou <joe.zhoufz@gmail.com>
2020-01-28 17:26:19 -0500

    Update BlackLitterman.py

--- BlackLitterman.py
+++ BlackLitterman.py
@@ -1,4 +1,18 @@
 import numpy as np
 import pandas as pd
 
+######################### testing inputs, will be replaced
+Sig = np.array([[1, 0.1, 0.1],
+                [0.1, 1, 0.1],
+                [0.1, 0.1, 1]])
 
+mu = np.array([1, 2, 3])
+
+lam = 1
+
+w_blInput = np.array([0.1, 0.4, 0.5])
+
+########################
+
+
+Pi

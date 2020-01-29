d2536993da62b99575a896bbabe7be112dd19d92
Fuzhong Zhou <40592542+fuzhongzhou@users.noreply.github.com>
2020-01-28 21:43:01 -0500

    11

--- BlackLitterman.py
+++ BlackLitterman.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+from numpy.linalg import inv
+from scipy.optimize import minimize, LinearConstraint
 
 ######################### testing inputs, will be replaced
 Sig = np.array([[1, 0.1, 0.1, 0.1],
@@ -7,18 +9,31 @@ Sig = np.array([[1, 0.1, 0.1, 0.1],
                 [0.1, 0.1, 1, 0.1],
                 [0.1, 0.1, 0.1, 1]])
 
-mu = np.array([0.01, 0.02, 0.03, 0.025]).reshape((-1, 1))
+ER = np.array([0.01, 0.02, 0.03, 0.025]).reshape((-1, 1))
+rf = 0.01
+l = np.ones((Sig.shape[0], 1))
 
-lam = 1
 
+
+
+
+# compute market portfolio
+A = l.T.dot(inv(Sig)).dot(ER)
+B = ER.T.dot(inv(Sig)).dot(ER)
+C = l.T.dot(inv(Sig)).dot(l)
+
+w_mkt = inv(Sig).dot(ER - rf * l) / (A - C * rf)
+mu = ER.T.dot(w_mkt)
+lam = (mu - rf) / (ER - rf * l).T.dot(inv(Sig)).dot(ER - rf * l)
+#w_capm = lam * inv(Sig).dot(ER - rf * l)
+
+# input from riskparity
 w_blInput = np.array([0.1, 0.4, 0.3, 0.2]).reshape((-1, 1))
-
 ########################
 
 # Black Litterman Parameters
 
 tau = 0.05
-
 
 # View parameters
 
@@ -34,18 +49,22 @@ Omeg = np.array([[1]])
 
 Pi = lam * Sig.dot(w_blInput)
 
-mu_BL_1 = np.linalg.inv(tau * Sig) + P.T.dot(Omeg).dot(P)
-mu_BL_2 = np.linalg.inv(tau * Sig).dot(mu) + P.T.dot(Omeg).dot(Q)
-mu_BL = np.linalg.inv(mu_BL_1).dot(mu_BL_2)
+ER_BL_1 = inv(tau * Sig) + P.T.dot(Omeg).dot(P)
+ER_BL_2 = inv(tau * Sig).dot(ER) + P.T.dot(Omeg).dot(Q)
+ER_BL = inv(ER_BL_1).dot(ER_BL_2)
 
-#mu_BL100 = Pi + tau * S
+mu_BL100 = Pi + tau * S
 
-Sig_BL = np.linalg.inv(mu_BL_1)
+Sig_BL = inv(ER_BL_1)
 
 
 # New mean variance analysis
-from scipy.optimize import minimize
 
-
+def lossfunc(x):
+    x = x.T
+    return -(x.T.dot(ER - rf * l) - 1/lam * x.T.dot(Sig).dot(x) / 2)
+re = minimize(lossfunc, w_mkt.T)
+print(re)
 
 # equity 的比重要保证 subject to
+pass

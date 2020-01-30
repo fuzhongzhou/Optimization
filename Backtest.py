027c4c060fd125622a18d8fe1ab9ceddca97ca39
erranl <42535150+erranl@users.noreply.github.com>
2020-01-29 19:17:43 -0500

    Update Backtest.py

--- Backtest.py
+++ Backtest.py
@@ -16,43 +16,48 @@ import matplotlib.pyplot as plt
 pool = pd.read_csv("pool.csv")
 pool["Date"] = pd.to_datetime(pool["Date"])
 pool = pool.set_index("Date")
-pool = pool[["Wells Fargo C&B Large Cap Value A", "Metropolitan West Total Return Bd I",
-            "Vanguard Real Estate ETF", "iShares Gold Trust"]]
 pool.head()
 
 
 
-def back_test(start, end):
+def back_test(start, end, commission_fee):
     initial_capital = 10000
     cycle = 12 #transfer frequency, yearly: 12, quaterly: 4
     
     def one_period_trade(t, capital, weight):
-        price = pool.iloc[t]
-        position = capital * weight / np.array(pool.iloc[t])
-        if t + cycle < end:   #if within back test period
-            capital = position.dot(pool.iloc[t + cycle])
+        weight = np.array(weight)                                                #target weight
+        price = np.array(pool.iloc[t])                                           #current price
+        old_position = position_list[-1]                                         #current position
+        commission = np.sum(np.abs(capital*weight - old_position*price))*commission_fee  
+        capital -= commission                                                   
+        position = capital*weight/price
+        if t+cycle<end:   #if within back test period 
+            next_capital = position.dot(pool.iloc[t+cycle])
         else:             #if not
-            capital = position.dot(pool.iloc[end])
-        return capital
-    
-    #WARNING:just for test, check using 4th fund price
-    weight = np.array([0, 0, 0, 1])
+            next_capital = position.dot(pool.iloc[end])
+        position_list.append(position)                                           #record the position over time
+        weight_list.append(weight)                                               #record the weight over time
+        return next_capital
     
     t = start
     wealth = initial_capital
     
     #records for wealth over time
     cumulative_value = pd.Series({pool.index[t]:wealth})
-
-    # at time t, change position, compute the return between (t, t + cycle)
-    while t < end:
+    weight_list = [np.array([0, 0, 0, 0, 0])]
+    position_list = [np.array([0, 0, 0, 0, 0])] 
+    
+    while(t<end):
         '''
-        Hundreds of codes for strategy which generate the new weight
+        Hundreds of codes for strategy that generates the new weight
+        '''
         
-        先跑一遍，把各个时刻的weight都记下来，然后back test直接按着记好的信号搞就可以
-        '''
+        #WARNING:just for test, check using 5th fund price
+        weight = [0, 0, 0, 0, 1]
+        
+        
         wealth = one_period_trade(t, wealth, weight)
-        cumulative_value[pool.index[t + cycle]] = wealth
+        cumulative_value[pool.index[t]] = wealth
         t += cycle
         
     return cumulative_value
@@ -61,6 +66,8 @@ def back_test(start, end):
 # In[55]:
 
 
-result = back_test(0, len(pool)-1)
+commission_fee = 0.0002
+start = 0            #start date, use the first ordinal number of date index
+end = len(pool)-1    #end date, use the last ordinal number date index
+result = back_test(start, end, commission_fee)
 plt.plot(result)
-
